8a3314301e2420360c3fbf57767342ec5f1f781a
leoth <leoth@L-3>
2024-08-08 15:55:25 -0400

    Add project files.

---
+++ UdemyThreadingCourse/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UdemyThreadingCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
